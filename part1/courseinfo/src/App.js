29a93c041b7872483a1c6b99497947f5ffe426f7
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-03-23 23:07:46 +0530

    Add files via upload

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const App = () => {
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
